bc71f4aa23f6592e03437a1be0044b4c7f13b9da
Patrick <57241652+patrick-duque@users.noreply.github.com>
2023-09-08 21:47:27 +0800

    feat: allow comma at the end of action

--- src/js/utils/convertStringToObject.js
+++ src/js/utils/convertStringToObject.js
@@ -3,10 +3,11 @@ const convertStringToObject = (string) => {
     const plainText = JSON.stringify(string.replace(/\n/g, ''));
     const doubleQuotes = plainText.replace(/\s/g, '').replace(/'/g, '"');
     const jsonText = doubleQuotes
-      .trim()
-      .replace(/(\w+:)|(\w+ :)/g, (matchedStr) => '"' + matchedStr.substring(0, matchedStr.length - 1) + '":')
-      .slice(1, -1);
-    const obj = JSON.parse(jsonText);
+      .trim() // remove all empty spaces in start and end
+      .replace(/,\s*(?=[\]}])/g, '') // remove all comma beside the closing bracket } or ]
+      .replace(/(\w+:)|(\w+ :)/g, (matchedStr) => '"' + matchedStr.substring(0, matchedStr.length - 1) + '":') // make the object key into a JSON key
+      .slice(1, -1); // remove the quotation marks at the start and end
+    const obj = JSON.parse(jsonText); // parse to JSON object
 
     return obj;
   } catch (error) {
