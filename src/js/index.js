813196197b069c9d08f6be9281c51965580cecca
Patrick <57241652+patrick-duque@users.noreply.github.com>
2023-09-08 16:04:50 +0800

    feat: added json viewer and editor

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,64 @@
 import '../styles/styles.css';
+import JSONFormatter from 'json-formatter-js';
+import { EditorState } from '@codemirror/state';
+import { EditorView } from '@codemirror/view';
+import { basicSetup } from 'codemirror';
 
-console.log('init');
+// import
+
+const dummyState = {
+  state0: {
+    test: '1234',
+    test2: '1234',
+  },
+  state1: {
+    test: '1234',
+    test2: '1234',
+  },
+  state2: {
+    test: '1234',
+    test2: '1234',
+  },
+  state3: {
+    test: '1234',
+    test2: '1234',
+    test3: [1, 2, 3, 4],
+  },
+};
+
+const formatter = new JSONFormatter(dummyState, {
+  animateOpen: true,
+  animateClose: true,
+});
+
+document.getElementById('json-container').appendChild(formatter.render());
+
+const state = EditorState.create({
+  doc: `{
+  type: '',
+  payload: ''
+}`,
+  extensions: [basicSetup, EditorState.tabSize.of(2)],
+});
+
+const codeEditor = new EditorView({
+  state,
+  parent: document.querySelector('#action-payload'),
+});
+
+const button = document.getElementById('fire');
+
+const dispatch = () => {
+  try {
+    const plainText = JSON.stringify(codeEditor.state.doc.toString().replace(/\n/g, ''));
+    const noSpaces = plainText.replace(/\s/g, '');
+    const trim = noSpaces.trim();
+    console.log(JSON.parse(trim));
+    // console.log(codeEditor);
+  } catch (error) {
+    console.log(error);
+    alert('Invalid payload');
+  }
+};
+
+button.addEventListener('click', dispatch);
