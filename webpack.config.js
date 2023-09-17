14a41782b9364764c71511c0c96778912316516b
Patrick <57241652+patrick-duque@users.noreply.github.com>
2023-09-17 23:36:17 +0800

    feat: added bg image and logo

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: './src/js/index.js', // Your entry point
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -13,12 +14,24 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/', // Specify the output path for images
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: 'head',
+      template: 'src/index.html', // Path to your HTML template
+      inject: 'body', // Inject the script tag in the body
     }),
   ],
   devServer: {
